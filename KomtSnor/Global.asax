9e28c8091583d3aebd2b1b8dda0f5e6465908635
sven alem <sven alem>
2016-05-29 21:04:10 +0200

    First commit

---
+++ KomtSnor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KomtSnor.MvcApplication" Language="C#" %>
---
+++ KomtSnor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KomtSnor.Startup))]
+namespace KomtSnor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
