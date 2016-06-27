9e3ef8a58af9650dc21c7856aab91233b77c09d3
sven alem <sven alem>
2016-06-28 00:35:06 +0200

    added Angular and examples to the project

---
+++ KomtSnor.Tests/Controllers/LoginControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using KomtSnor.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KomtSnor.Tests.Controllers
+{
+    [TestClass]
+    public class LoginControllerTest
+    {
+
+        [TestMethod]
+        public void SQLServerLogin()
+        {
+            string valueOne = "UnKnown";
+            string valueTwo = "UnKnown";
+
+            Assert.AreEqual(valueOne, valueTwo);
+        }
+    }
+}
---
+++ KomtSnor/Controllers/AngularController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KomtSnor.Controllers
+{
+    public class AngularController : Controller
+    {
+        // GET: Angular
+        public ActionResult AngularJS1()
+        {
+            return View();
+        }
+    }
+}
